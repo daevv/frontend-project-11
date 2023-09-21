cb13f9a436480a1209c5de011d2112cbcebee9ce
daevv <dediaev.danil130702@gmail.com>
2023-09-21 19:57:22 +0300

    remove scss from project, add some htnl code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.css';
 import 'bootstrap';
 
 console.log('Hello World!');
--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ const config = {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
