13eaf95032a77d6a8d6605e9b85155e299c2098d
daevv <dediaev.danil130702@gmail.com>
2023-09-27 23:34:22 +0300

    add ru.js for i18next but it doesnt work

---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    posts: 'Посты',
+    feeds: 'Фиды',
+    openFeedButton: 'Просмотр',
+    reedFullPostButton: 'Читать полностью',
+    closeButton: 'Закрыть',
+    success: 'RSS успешно загружен',
+    errors: {
+      validation: {
+        wrongFormat: 'Ссылка должна быть валидным URL',
+        sameFeed: 'RSS уже существует',
+      },
+    },
+  },
+};
