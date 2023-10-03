429c49a7e6341d84059f168c04965de953af593c
daevv <dediaev.danil130702@gmail.com>
2023-10-03 19:54:52 +0300

    get rid of extra code

--- src/rss.js
+++ src/rss.js
@@ -1,18 +1,11 @@
-import updateFeeds from './updateFeed';
-
-export default (res, state) => {
+export default (link) => {
   const parser = new DOMParser();
   const parse = (data) => parser.parseFromString(data, 'text/xml');
-  fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(res)}`)
+  return fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(link)}`)
     .then((response) => {
       if (response.ok) return response.json();
       throw new Error('Network error');
     })
-    .then((data) => data.contents)
-    .then((content) => updateFeeds(parse(content), state))
-    .catch((err) => {
-      state.status = 'input';
-      state.error = err.message;
-      state.feedbackStatus = 'error';
-    });
+    .then((data) => parse(data.contents))
+    .catch(() => false);
 };
