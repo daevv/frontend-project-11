b667cff829f6df3951db46edd472bc83f2d68495
daevv <dediaev.danil130702@gmail.com>
2023-10-03 19:53:28 +0300

    change onChange logic, split render function

--- src/view.js
+++ src/view.js
@@ -9,43 +9,52 @@ export default (state, elements, i18n) => {
     postsContainer,
   } = elements;
 
-  const renderFeeds = (feeds) => {
-    const feedNames = feeds.map(({ title, description }) => (`
-      <li class="list-group-item border-0 border-end-0">
-        <h3 class="h6 m-0">${title}</h3>
-        <p class="m-0 small text-black-50">${description}</p>
-      </li>`
-    )).join('');
+  const renderFeed = (feed) => {
+    const { title, description } = feed;
+    const feedElement = document.createElement('li');
+    feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
+    feedElement.innerHTML = `
+      <h3 class="h6 m-0">${title}</h3>
+      <p class="m-0 small text-black-50">${description}</p>`;
 
-    const posts = feeds.map((feed) => {
-      const currentFeedPosts = feed.posts;
-      return currentFeedPosts.map(({ postTitle, postLink, postId }) => (`
-        <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
-          <a href="${postLink}" class="fw-bold" data-id="${postId}" target="_blank" rel="noopener noreferrer">${postTitle}</a>
-          <button type="button" class="btn btn-outline-primary btn-sm" data-id="${postId}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>
-        </li>`
-      )).join('');
-    }).join('');
-
-    feedsContainer.innerHTML = `
+    if (!feedsContainer.firstElementChild) {
+      feedsContainer.innerHTML = `
       <div class="card border-0">
         <div class="card-body">
           <h2 class="card-title h4">Фиды</h2>
         </div>
         <ul class="list-group border-0 rounded-0">
-          ${feedNames}
         </ul>
       </div>`;
+    }
+    const feedsList = feedsContainer.querySelector('ul');
+    feedsList.insertBefore(feedElement, feedsList.firstElementChild);
+  };
 
-    postsContainer.innerHTML = `
+  const renderPosts = (posts) => {
+    if (!postsContainer.firstElementChild) {
+      postsContainer.innerHTML = `
       <div class="card border-0">
         <div class="card-body">
           <h2 class="card-title h4">Посты</h2>
         </div>
         <ul class="list-group border-0 rounded-0">
-          ${posts}
         </ul>
       </div>`;
+    }
+    const postsList = postsContainer.querySelector('ul');
+
+    // возможно придётся реверснуть, чтобы изменить порядок
+    const html = posts.map((post) => {
+      const { postTitle, postLink, postId } = post;
+      return `
+        <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+          <a href="${postLink}" class="fw-bold" data-id="${postId}" target="_blank" rel="noopener noreferrer">${postTitle}</a>
+          <button type="button" class="btn btn-outline-primary btn-sm" data-id="${postId}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>
+        </li>`;
+    }).join('');
+
+    postsList.innerHTML = html + postsList.innerHTML;
   };
 
   const handleStatusChange = (status) => {
@@ -79,16 +88,19 @@ export default (state, elements, i18n) => {
 
   const watchedState = onChange(state, (path, value, previousValue) => {
     switch (path) {
-      case 'status':
+      case 'form.status':
         handleStatusChange(value);
         break;
-      case 'error':
+      case 'form.error':
         handleError(value, previousValue);
         break;
-      case 'data':
-        renderFeeds(value);
+      case 'data.feeds':
+        renderFeed(state.data.lastFeed);
         break;
-      case 'feedbackStatus':
+      case 'data.posts':
+        renderPosts(state.data.lastPosts);
+        break;
+      case 'form.feedbackStatus':
         handleFeedbackStatusChange(value);
         break;
       default:
