1dff0bd3a2d6b47e612a4050373d340bb09a22b1
daevv <dediaev.danil130702@gmail.com>
2023-10-10 21:43:35 +0300

    change promises handling

--- src/api/checkForUpdates.js
+++ src/api/checkForUpdates.js
@@ -1,19 +1,21 @@
 import parsePosts from '../parsers/parsePosts';
 import rss from './rss';
 
-const checkForUpdates = (state) => setTimeout(() => {
+const checkForUpdates = (state) => {
   const { feeds } = state.data;
   console.log('UPDATES CHECKING!!!');
-  feeds.forEach((feed) => {
+  const requests = feeds.map((feed) => {
     const { id, url } = feed;
     const previousPostsTitles = state.data.posts
       .filter(({ feedId }) => feedId === id)
       .map(({ postTitle }) => postTitle);
-    rss(url)
-      .then((response) => [...response.querySelectorAll('item')])
-      .then((postItems) => parsePosts(postItems, id))
-      .then((currentPosts) => currentPosts
-        .filter(({ postTitle }) => !previousPostsTitles.includes(postTitle)))
+    const request = rss(url).then((response) => {
+      const postItems = [...response.querySelectorAll('item')];
+      const parsedPosts = parsePosts(postItems, id);
+      const newPosts = parsedPosts
+        .filter(({ postTitle }) => !previousPostsTitles.includes(postTitle));
+      return newPosts;
+    })
       .then((newPosts) => {
         if (newPosts.length > 0) {
           console.log('there are new posts');
@@ -24,8 +26,9 @@ const checkForUpdates = (state) => setTimeout(() => {
         console.log('there are no new posts');
       })
       .catch(() => {});
+    return request;
   });
-  checkForUpdates(state);
-}, 5000);
+  Promise.all(requests).finally(() => setTimeout(() => checkForUpdates(state), 5000));
+};
 
 export default checkForUpdates;
