aacaf335a7974ce20fbfadff098a2648098e229a
daevv <dediaev.danil130702@gmail.com>
2023-10-13 19:13:21 +0300

    change html-elements creation logic

--- src/view.js
+++ src/view.js
@@ -9,47 +9,75 @@ export default (state, elements, i18n) => {
     postsContainer,
   } = elements;
 
+  const getCardElement = (type) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card', 'border-0');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const listElement = document.createElement('ul');
+    listElement.classList.add('list-group', 'border-0', 'rounded-0');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title', 'h4');
+    cardTitle.textContent = i18n.t(type);
+
+    cardBody.append(cardTitle);
+    cardElement.append(cardBody);
+    cardElement.append(listElement);
+
+    return cardElement;
+  };
+
   const renderFeed = (feed) => {
     const { title, description } = feed;
+
     const feedElement = document.createElement('li');
     feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
-    feedElement.innerHTML = `
-      <h3 class="h6 m-0">${title}</h3>
-      <p class="m-0 small text-black-50">${description}</p>`;
+
+    const feedTitle = document.createElement('h3');
+    feedTitle.classList.add('h6', 'm-0');
+    feedTitle.textContent = title;
+
+    const feedSubTitle = document.createElement('p');
+    feedSubTitle.classList.add('m-0', 'small', 'text-black-50');
+    feedSubTitle.textContent = description;
+
+    feedElement.append(feedTitle);
+    feedElement.append(feedSubTitle);
 
     if (!feedsContainer.firstElementChild) {
-      feedsContainer.innerHTML = `
-      <div class="card border-0">
-        <div class="card-body">
-          <h2 class="card-title h4">${i18n.t('feeds')}</h2>
-        </div>
-        <ul class="list-group border-0 rounded-0">
-        </ul>
-      </div>`;
+      const cardElement = getCardElement('feeds');
+      feedsContainer.append(cardElement);
     }
+
     const feedsList = feedsContainer.querySelector('ul');
     feedsList.insertBefore(feedElement, feedsList.firstElementChild);
   };
 
   const renderPosts = ({ lastPosts, readPostsIds }) => {
     if (!postsContainer.firstElementChild) {
-      postsContainer.innerHTML = `
-      <div class="card border-0">
-        <div class="card-body">
-          <h2 class="card-title h4">${i18n.t('posts')}</h2>
-        </div>
-        <ul class="list-group border-0 rounded-0">
-        </ul>
-      </div>`;
+      const cardElement = getCardElement('posts');
+      postsContainer.append(cardElement);
     }
     const postsList = postsContainer.querySelector('ul');
 
-    // возможно придётся реверснуть, чтобы изменить порядок
-    lastPosts.forEach((post) => {
+    lastPosts.reverse().forEach((post) => {
       const { postTitle, postLink, postId } = post;
+
       const listElement = document.createElement('li');
       listElement.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-      listElement.innerHTML = `<a href="${postLink}" class="${readPostsIds.includes(postId) ? 'fw-normal' : 'fw-bold'}" data-id="${postId}" target="_blank" rel="noopener noreferrer">${postTitle}</a>`;
+
+      const title = document.createElement('a');
+      title.classList.add(readPostsIds.includes(postId) ? 'fw-normal' : 'fw-bold');
+      title.href = postLink;
+      title.dataset.id = postId;
+      title.setAttribute('target', '_blank');
+      title.setAttribute('rel', 'noopener noreferrer');
+      title.textContent = postTitle;
+
+      listElement.append(title);
 
       const viewButton = document.createElement('button');
       viewButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
@@ -74,38 +102,33 @@ export default (state, elements, i18n) => {
     submitButton.disabled = status !== 'input';
   };
 
-  const handleError = (error, previousError) => {
-    const errorText = error ? i18n.t(error) : i18n.t('success');
-    feedbackField.textContent = errorText;
-    if (error && previousError) {
-      return;
-    }
-    if (error) {
-      field.classList.add('is-invalid');
-      field.classList.remove('is-valid');
-    } else {
-      field.classList.remove('is-invalid');
-      field.classList.add('is-valid');
-    }
+  const handleFeedbackChange = (value) => {
+    const feedbackText = i18n.t(value);
+    console.log(feedbackText);
+    feedbackField.textContent = feedbackText;
   };
 
   const handleFeedbackStatusChange = (status) => {
     if (status === 'success') {
       feedbackField.classList.add('text-success');
       feedbackField.classList.remove('text-danger');
+      field.classList.remove('is-invalid');
+      field.classList.add('is-valid');
     } else {
       feedbackField.classList.remove('text-success');
       feedbackField.classList.add('text-danger');
+      field.classList.add('is-invalid');
+      field.classList.remove('is-valid');
     }
   };
 
-  const watchedState = onChange(state, (path, value, previousValue) => {
+  const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'form.status':
         handleStatusChange(value);
         break;
-      case 'form.error':
-        handleError(value, previousValue);
+      case 'form.feedback':
+        handleFeedbackChange(value);
         break;
       case 'data.feeds':
         renderFeed(state.data.lastFeed);
